8e01a8f74b492b4bca555c7dc93bee4a47684651
unknown <ninadc32@gmail.com>
2022-11-10 15:11:54 +0530

    color and project container  changes

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -4,32 +4,34 @@ import LaunchIcon from '@material-ui/icons/Launch'
 import './ProjectContainer.css'
 
 const ProjectContainer = ({ project }) => (
-  <div className='project'>
-    <h3 className='project__name'>{project.name}</h3>
+  <a href={project.sourceCode}>
+    <div className='project'>
+      <h3 className='project__name'>{project.name}</h3>
 
-    <p className='project__description'>{project.description}</p>
-    {project.stack && (
-      <ul className='project__stack'>
-        {project.stack.map((item) => (
-          <li key={uniqid()} className='project__stack-item'>
-            {item}
-          </li>
-        ))}
-      </ul>
-    )}
+      <p className='project__description'>{project.description}</p>
+      {project.stack && (
+        <ul className='project__stack'>
+          {project.stack.map((item) => (
+            <li key={uniqid()} className='project__stack-item'>
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
 
-    {project.sourceCode && (
-      <a
-        href={project.sourceCode}
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label='source code'
-        className='link link--icon'
-      >
-        <GitHubIcon />
-      </a>
-    )}
-  </div>
+      {/* {project.sourceCode && (
+        <a
+          href={project.sourceCode}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label='source code'
+          className='link link--icon'
+        >
+          <GitHubIcon />
+        </a>
+      )} */}
+    </div>
+  </a>
 )
 
 export default ProjectContainer
